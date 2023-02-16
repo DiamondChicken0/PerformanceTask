ed1f8428e426d699ced99769bb468157c7782e33
Simon <47757232+DiamondChicken0@users.noreply.github.com>
2023-02-16 13:11:12 -0500

    amndja

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ py.display.set_caption("Cool Game")
 
 skyBlue = (89, 247, 255)
 oceanBlue = (89, 136, 255)
+darkBlue = (0, 51, 204)
 sandYellow = (255, 249, 128)
 sandOutline = (173, 169, 87)
+grassGreen = (102, 255, 102)
+darkGreen = (51, 153, 51)
+woodBrown = (153, 102, 51)
 
 class States(Enum):
     mainMenu = 1
@@ -28,11 +32,37 @@ while running:
     
     if state == States.mainMenu:
         introRunning = True
+        j = 0
         while introRunning:
+            if j < 570:
+                j = j + 2.5
             py.draw.rect(screen, skyBlue, skyBG)
+            pydraw.box(screen, ((0,400), (1280,500)), grassGreen)
+
+            pydraw.filled_ellipse(screen, 0, 550, 215, 215, darkGreen)
+            pydraw.filled_ellipse(screen, 150, 550, 315, 265, darkGreen)
+            pydraw.filled_ellipse(screen, 400, 500, 265, 415, darkGreen)
+            pydraw.filled_ellipse(screen, 750, 550, 215, 315, darkGreen)
+            pydraw.filled_ellipse(screen, 1100, 550, 415, 415, darkGreen)
+
+            pydraw.filled_ellipse(screen, 0, 550, 200, 200, grassGreen)
+            pydraw.filled_ellipse(screen, 150, 550, 300, 250, grassGreen)
+            pydraw.filled_ellipse(screen, 400, 500, 250, 400, grassGreen)
+            pydraw.filled_ellipse(screen, 750, 550, 200, 300, grassGreen)
+            pydraw.filled_ellipse(screen, 1100, 550, 400, 400, grassGreen)
+
+            pydraw.filled_ellipse(screen, 640, 680, 1510, 210, sandOutline)
             pydraw.filled_ellipse(screen, 640, 680, 1500, 200, sandYellow)
+
+            for i in range(0,33):
+                pydraw.filled_ellipse(screen, i * 40, 720, 60, 110, darkBlue)
             for i in range(0,33):
                 pydraw.filled_ellipse(screen, i * 40, 720, 50, 100, oceanBlue)
+            
+            pydraw.box(screen, ((190, j - 530), (900,520)), (0,0,0))
+            pydraw.box(screen, ((200, j - 520), (880,500)), woodBrown)
+            
+            
             
             
 
