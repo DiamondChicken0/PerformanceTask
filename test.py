227f1154c1b39e239fc3e24fdf119a9e79a14996
Simon <47757232+DiamondChicken0@users.noreply.github.com>
2023-03-22 14:09:37 -0400

    h

--- test.py
+++
@@ -1,9 +0,0 @@
-from synthesizer import Player, Synthesizer, Waveform
-
-player = Player()
-player.open_stream()
-
-print("play major chord")
-synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=False)
-chord = ["C4", "E4", "G4"]
-player.play_wave(synthesizer.generate_chord(chord, 3.0))
